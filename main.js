6101c8e025526676d58188d2ad093942062a6d1c
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-03-02 16:27:29 +0200

    destructuring props practice

---
+++ main.js
@@ -0,0 +1,9 @@
+const employee = {
+      firstName: "Ramesh",
+      lastName: "Fadatare",
+      emailId: "ramesh@gmail.com",
+    };
+  //  const { firstName, lastName, emailId } = employee;
+  //  console.log(firstName);
+
+  console.log({ employee });
